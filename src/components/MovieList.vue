c12a66f3a301215331873e20f319faca3eecddc2
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-14 19:08:10 +0200

    MovieList sets an array for search result

---
+++ src/components/MovieList.vue
@@ -0,0 +1,24 @@
+<script>
+import MovieItem from './MovieItem.vue'
+
+export default {
+    components: {
+        MovieItem
+    },
+    props: {
+        movies: {
+            type: Array,
+            required: true,
+            default:() => []
+        }
+    }
+};
+
+</script>
+<template>
+    <div v-for="movie in movies" :key="movie.id">
+        {{ console.log(movie) }}
+        <MovieItem :movie="movie" />
+    </div>
+</template>
+<style></style>
