1bffe13f15c8693e06597482ad71516f02aa259b
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-14 12:02:32 +0200

    set a method to get data via axios from "themoviedb.org"

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script setup>
+  export default{
+    components:{
 
+    }
+  },
+  data(){
+    return{
+      movies: []
+    };
+  },
+  methods:{
+    async searchMovies(query) {
+      try{
+        const response = await axios.get('https://api.themoviedb.org/3/search/movie'), {
+          params: {
+            api_key: '62e3cbd4b6904ca7307e4b3b03e03a2a',
+            query: query,
+            lenguage: 'it-IT'
+          }
+        }
+      }
+    }
+  }
 </script>
 
 <template>
+  <div id="app">
 
+  </div>
 </template>
 
 <style scoped>
