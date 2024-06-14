3d743cbee110391dbfd528d91291cc281ff6f264
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-14 16:04:55 +0200

    set a component in vue for the searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        data() {
+            return {
+                query: ''
+            };
+        },
+        methods: {
+            search() {
+                this.$emit('search', this.query);
+            }
+        }
+    };
+</script>
+
+<template>
+    <div>
+        <input v-model="query" @keyup.enter="search" placeholder="Cerca un film..."/>
+        <button @click="search">Cerca</button>
+    </div>
+</template>
