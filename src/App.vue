97e4c1abd5751486b51a156d2ca2c14ab748a9a1
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-14 18:30:23 +0200

    deleted link to css style sheet

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
-<script setup>
-  export default{
-    components:{
+<script>
+import axios from 'axios';
+import SearchBar from './components/SearchBar.vue';
+import MovieList from './components/MovieList.vue';
 
-    }
+export default {
+  components: {
+    SearchBar,
+    MovieList
   },
-  data(){
-    return{
+  data() {
+    return {
       movies: []
     };
   },
-  methods:{
+  methods: {
     async searchMovies(query) {
-      try{
-        const response = await axios.get('https://api.themoviedb.org/3/search/movie'), {
+      try {
+        const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
           params: {
             api_key: '62e3cbd4b6904ca7307e4b3b03e03a2a',
             query: query,
-            lenguage: 'it-IT'
+            language: 'it-IT'
           }
-        }
+        });
+        this.movies = response.data.results;
+      } catch (error) {
+        console.error('Error searching movies:', error);
       }
     }
   }
+};
 </script>
 
 <template>
   <div id="app">
-
+    <SearchBar @search="searchMovies"/>
+    <MovieList :movies="movies"/>
   </div>
 </template>
 
