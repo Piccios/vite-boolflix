41239dc36b2293f9f81a829f89208d77aab888db
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-14 19:06:51 +0200

    Component now shows a card with movie's title, lenguage and average vote

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,27 @@
+<script>
+    export default {
+        props: {
+            movie: {
+                type: Object,
+                required: true
+            }
+        }
+    };
+</script>
+
+<template>
+    <div class="movie-item">
+        <h3>{{ movie.title }}</h3>
+        <p>{{ movie.original_language }}</p>
+        <p>{{ movie.vote_average }}</p>
+    </div>
+</template>
+
+<style scoped>
+    .movie-item{
+        margin-bottom: 20px;
+        padding: 10px;
+        border: 1px solid black;
+        border-radius: 5px;
+    }
+</style>
