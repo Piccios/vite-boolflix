40889dc52118beb02a55b1a8140c98098fa9b657
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-17 13:02:42 +0200

    installed package "flag-icons" to show original language flag

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,19 +1,36 @@
 <script>
-    export default {
-        props: {
-            movie: {
-                type: Object,
-                required: true
-            }
+import { countries } from 'country-flag-icons';
+
+export default {
+    props: {
+        movie: {
+            type: Object,
+            required: true
         }
-    };
+    },
+    methods: {
+        getFlagUrl(language) {
+            const languageToCountryMap = {
+                'en': 'gb',
+                'it': 'it',
+                'fr': 'fr',
+                'de': 'de',
+                'es': 'es',
+                'ja': 'jp',
+                'ko': 'kr',
+                'zh': 'cn'
+            };
+            const countryCode = languageToCountryMap[language] || 'un';
+            return `https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.6/flags/4x3/${countryCode}.svg`;
+        }
+    }}
 </script>
 
 <template>
     <div class="movie-item">
         <p><strong>Movie's Title:</strong>{{' ' + movie.title }}</p>
         <p><strong>Movie's original title: </strong>{{' ' + movie.original_title }}</p>
-        <p><strong>Original lenguage: </strong>{{' ' + movie.original_language }}</p>
+        <p><strong>Original lenguage: </strong> <img :src="getFlagUrl(movie.original_language)" :alt="movie.original_language"></p>
         <p><strong>Average review: </strong>{{' ' + movie.vote_average }}</p>
     </div>
 </template>
@@ -28,5 +45,10 @@
 
     div.movie-item p strong{
         text-decoration: underline;
+    }
+
+    img {
+        height: 20px;
+        margin-left: 1.5rem;
     }
 </style>
