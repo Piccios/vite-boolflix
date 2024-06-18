1e53efc6d0b37fde26a56b6bb6eed7930c352ee2
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-18 11:31:41 +0200

    movies and series poster is now shown in page

--- src/components/MediaItem.vue
+++ src/components/MediaItem.vue
@@ -1,11 +1,18 @@
 <script>
-import { countries } from 'country-flag-icons';
+// import { countries } from 'country-flag-icons';
 
 export default {
     props: {
         item: {
             type: Object,
             required: true
+        }
+    },
+    computed: {
+        imageUrl (){
+            const baseUrl = 'https://image.tmdb.org/t/p/';
+            const size = 'w500';
+            return `${baseUrl}${size}${this.item.poster_path}`
         }
     },
     methods: {
@@ -28,9 +35,10 @@ export default {
 
 <template>
     <div class="item">
+        <img :src="imageUrl" :alt="item.title || item.name">
         <p><strong>{{ item.type === 'movie' ? "Movie's Title:" : "Show's Title:" }}</strong>{{' ' + (item.title || item.name) }}</p>
         <p><strong>{{ item.type === 'movie' ? "Movie's original title:" : "Show's original name:" }}</strong>{{' ' + (item.original_title || item.original_name) }}</p>
-        <p><strong>Original lenguage: </strong> <img :src="getFlagUrl(item.original_language)" :alt="item.original_language"></p>
+        <p><strong>Original lenguage: </strong> <img class="flag" :src="getFlagUrl(item.original_language)" :alt="item.original_language"></p>
         <p><strong>Average review: </strong>{{' ' + item.vote_average }}</p>
     </div>
 </template>
@@ -47,8 +55,9 @@ export default {
         text-decoration: underline;
     }
 
-    img {
+    img.flag{
         height: 20px;
         margin-left: 1.5rem;
     }
+
 </style>
